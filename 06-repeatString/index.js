d0d81273481265081a6da5b460e8e9801708096e
Yurii <y.nadilnyi@gmail.com>
2021-09-09 18:26:16 +0300

    ex-6 it was so hard

--- 06-repeatString/index.js
+++ 06-repeatString/index.js
@@ -4,5 +4,9 @@
  * @returns {string} - returns the new string
  */
 export const repeatString = (str = '') => {
+	let one = str.split("");
+	let two = one.map((ch, i) => (ch = ch.toLowerCase().repeat(i + 1)) && ch.charAt(0).toUpperCase() + ch.slice(1));
+	let newString = two.join('-');
 
+	return newString;
 }
