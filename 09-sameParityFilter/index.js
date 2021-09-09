89fd1b502207fc48b7acd4cb2cfee0fb1cb6ac83
Yurii <y.nadilnyi@gmail.com>
2021-09-09 21:18:29 +0300

    ex-9

--- 09-sameParityFilter/index.js
+++ 09-sameParityFilter/index.js
@@ -1,3 +1,18 @@
 export const sameParityFilter = (arr = []) => {
+	let newArrayOdd = [];
+	let newArrayEven = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
+			newArrayEven.push(arr[i])
+
+		} else {
+			newArrayOdd.push(arr[i])
+		}
+	}
+	if (arr[0] % 2 == 0) {
+		return newArrayEven
+	} else {
+		return newArrayOdd
+	}
 
 }
