66db79131661c3cc83b3a574a4dafe2855c38f72
Yurii <y.nadilnyi@gmail.com>
2021-09-09 21:18:57 +0300

    ex-7;8

--- 07-getLastWordLength/index.js
+++ 07-getLastWordLength/index.js
@@ -1,3 +1,5 @@
-export const getLastWordLength = str => {
-
+export const getLastWordLength = (str = "") => {
+	let newString = str.split(/[\,!]+/);
+	newString[newString.length - 1].length
+	return newString
 };
--- 08-chunk/index.js
+++ 08-chunk/index.js
@@ -1,5 +1,6 @@
 // TODO: rename function, maybe array-of-arrays
 
 export const chunk = (arr = [], size = 0) => {
-
+	let result = arr.reduce((rows, key, index) => (index % size == 0 ? rows.push([key]) : rows[rows.length - 1].push(key)) && rows, []);
+	return result;
 };
